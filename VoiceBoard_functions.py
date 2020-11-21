7194c6d66b9b1c4218bc254477e6e080a202a77e
GTPV <parangvo@gmail.com>
2020-11-21 10:19:53 +0900

    added VoiceBoard_main.py
    VoiceBoard_functions:
    cursorcontroller class added

---
+++ VoiceBoard_functions.py
@@ -0,0 +1,63 @@
+from pynput.keyboard import Key,Controller
+keyboard = Controller()
+
+
+def typeInput(stringToType):
+    keyboard.type(stringToType)
+
+class CursorController:
+        def Left(self, amountToMoveCursor):
+                for i in range(amountToMoveCursor):
+                        keyboard.press(Key.left)
+        def Right(self, amountToMoveCursor):
+                for i in range(amountToMoveCursor):
+                        keyboard.press(Key.right)
+        def Up(self, amountToMoveCursor):
+                for i in range(amountToMoveCursor):
+                        keyboard.press(Key.up)
+        def Down(self, amountToMoveCursor):
+                for i in range(amountToMoveCursor):
+                        keyboard.press(Key.down)
+
+Cursor = CursorController()
+
+
+'''
+def moveCursorLeft(amountToMoveCursor):
+        for i in range(amountToMoveCursor):
+                keyboard.press(Key.left)
+
+def moveCursorRight(amountToMoveCursor):
+        for i in range(amountToMoveCursor):
+                keyboard.press(Key.right)
+
+def moveCursorUp(amountToMoveCursor):
+    for i in range(amountToMoveCursor):
+        keyboard.press(Key.up)
+
+def moveCursorDown(amountToMoveCursor):
+    for i in range(amountToMoveCursor):
+        keyboard.press(Key.down)
+'''
+
+
+def typePrint():
+        typeInput("printf("")")
+        Cursor.Left(2)
+
+def defineVariable(variableType, variableName):
+        typeInput(variableType+" = "+variableName)
+
+
+def typeClassicIf(initialCondition, terminalCondition, changeCondition):
+        typeInput("if("+initialCondition+";"+terminalCondition+";"+changeCondition+"){}")
+        Cursor.Left(1)
+
+
+def typeRepsIf(reps):
+        typeInput("if(int i = 0; i < "+reps+"; i++){}")
+        Cursor.Left(1)
+
+def typeElse():
+        typeInput("else{}")
+        Cursor.Left(1)
