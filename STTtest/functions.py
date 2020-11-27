a75949fd6733afb83984e4a3f1a1a64f96ccdf3d
GTPV <parangvo@gmail.com>
2020-11-27 14:59:48 +0900

    modified stttest

--- STTtest/functions.py
+++ STTtest/functions.py
@@ -2,21 +2,26 @@ import speech_recognition as sr
 
 
 r = sr.Recognizer()
+print("r = sr.Recognizer()")
 
-Mic = sr.Microphone()
+mic = sr.Microphone()
 print("Mic = sr.Microphone()")
 
 
 
 
-with Mic as source:
-    r.adjust_for_ambient_noise(source, duration=1)
+with mic as source:
+    print("with mic as source")
+    #r.adjust_for_ambient_noise(source, 1)
+    #print("r.adjust_for_ambient_noise(source, 1)")
     print("mic input...")
-    audioInput = r.listen(source, 3)
+    audio = r.listen(source, timeout = 3)
+    print("audio = r.listen(source, timeout = 3)")
     print("Stop talking")
     try:
-        AudioInput = r.recognize_google(audioInput)
-        print(AudioInput)
+        text = r.recognize_google(audio)
+        print("text = r.recognize_google(audio)")
+        print(text)
     except:
         print("Can't recognize any voice")
 print("Program is over")
